9b551974d70579c322f1a74635457248b69599fb
Kayle Gishen <kaylegishen@gmail.com>
2023-02-28 17:02:15 -0500

    fix: pass query sub args to implicit lookup

--- __tests__/implicit-many-to-many.test.ts
+++ __tests__/implicit-many-to-many.test.ts
@@ -96,4 +96,75 @@ describe("Implicit Many-to-Many", () => {
       );
     });
   });
+
+  describe("nested", () => {
+    let client;
+    let folder;
+    beforeEach(async () => {
+      client = await createPrismaClient({
+        document: [
+          {
+            id: "doc1",
+            name: "Doc 1",
+          },
+          {
+            id: "doc2",
+            name: "Doc 2",
+          },
+        ],
+        tag: [
+          {
+            id: "tag1",
+            name: "Tag 1",
+          },
+          {
+            id: "tag2",
+            name: "Tag 2",
+          },
+          {
+            id: "tag3",
+            name: "Tag 3",
+          },
+        ],
+        folder: [
+          {
+            id: "folder1",
+            name: "Folder 1",
+          },
+        ],
+      });
+      await client.document.update({
+        where: { id: "doc1" },
+        data: {
+          tags: {
+            connect: [{ id: "tag1" }, { id: "tag2" }],
+          },
+        },
+        include: {
+          tags: true,
+        },
+      });
+      folder = await client.folder.update({
+        where: { id: "folder1" },
+        data: {
+          documents: {
+            connect: [{ id: "doc1" }],
+          },
+        },
+        include: {
+          documents: {
+            include: {
+              tags: true,
+            },
+          },
+        },
+      });
+    });
+
+    test("include returns all levels", () => {
+      expect(folder.documents).toHaveLength(1);
+      expect(folder.documents[0].tags).not.toBeUndefined();
+      expect(folder.documents[0].tags).toHaveLength(2);
+    });
+  });
 });
