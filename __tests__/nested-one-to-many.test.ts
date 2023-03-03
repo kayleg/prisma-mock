2a5c9e2f041827ea699c3311b93ef3fdbeb3d47d
Kayle Gishen <kaylegishen@gmail.com>
2023-03-02 22:48:32 -0500

    fix(one-to-many): fix for connecting one-to-many through many side of relationship

--- __tests__/nested-one-to-many.test.ts
+++ __tests__/nested-one-to-many.test.ts
@@ -1,439 +1,451 @@
 // @ts-nocheck
 
-import createPrismaClient from '../src'
+import createPrismaClient from "../src";
 
-describe('Create', () => {
-
-  test('create one-to-many', async () => {
-    const client = await createPrismaClient({})
+describe("Create", () => {
+  test("create one-to-many", async () => {
+    const client = await createPrismaClient({});
     // TODO: Check output
     const account = await client.account.create({
       data: {
         users: {
           create: {
             role: "USER",
-            uniqueField: 'user',
+            uniqueField: "user",
           },
-        }
+        },
       },
       include: {
-        users: true
-      }
-    })
-    const users = await client.user.findMany()
-    expect(users.length).toBe(1)
-    expect(account).toEqual(
-      {
-        id: 1,
-        name: null,
-        sort: null,
-        users: [{
+        users: true,
+      },
+    });
+    const users = await client.user.findMany();
+    expect(users.length).toBe(1);
+    expect(account).toEqual({
+      id: 1,
+      name: null,
+      sort: null,
+      users: [
+        {
           id: 1,
           accountId: 1,
           role: "USER",
           deleted: false,
           sort: null,
           clicks: null,
-          uniqueField: 'user',
-        }]
-      }
-    )
-  })
+          uniqueField: "user",
+        },
+      ],
+    });
+  });
 
-  test('create one-to-many (array)', async () => {
-    const client = await createPrismaClient({})
+  test("create one-to-many (array)", async () => {
+    const client = await createPrismaClient({});
     // TODO: Check output
     const account = await client.account.create({
       data: {
         users: {
-          create: [{
-            role: "USER",
-            uniqueField: 'user',
-          }, {
-            role: "ADMIN",
-            uniqueField: 'admin',
-          }],
-        }
+          create: [
+            {
+              role: "USER",
+              uniqueField: "user",
+            },
+            {
+              role: "ADMIN",
+              uniqueField: "admin",
+            },
+          ],
+        },
       },
       include: {
-        users: true
-      }
-    })
-    expect(account).toEqual(
-      {
-        id: 1,
-        name: null,
-        sort: null,
-        users: [{
+        users: true,
+      },
+    });
+    expect(account).toEqual({
+      id: 1,
+      name: null,
+      sort: null,
+      users: [
+        {
           id: 1,
           role: "USER",
           accountId: 1,
           deleted: false,
           clicks: null,
           sort: null,
-          uniqueField: 'user',
-        }, {
+          uniqueField: "user",
+        },
+        {
           id: 2,
           role: "ADMIN",
           accountId: 1,
           deleted: false,
           clicks: null,
           sort: null,
-          uniqueField: 'admin',
-        }]
-      }
-    )
-  })
-  test('createMany', async () => {
+          uniqueField: "admin",
+        },
+      ],
+    });
+  });
+  test("createMany", async () => {
     const client = await createPrismaClient({
       answers: [],
       userAnswers: [],
-      user: [
-        { id: 1, role: "USER" }
-      ]
-    })
+      user: [{ id: 1, role: "USER" }],
+    });
     // TODO: Check output
     const answer = await client.answers.create({
       data: {
         title: "Title",
         users: {
           createMany: {
-            data: [{ userId: 1 }]
+            data: [{ userId: 1 }],
           },
-        }
+        },
       },
       include: {
-        users: true
-      }
-    })
-    expect(answer).toEqual(
-      {
-        title: "Title",
-        id: 1,
-        users: [{
+        users: true,
+      },
+    });
+    expect(answer).toEqual({
+      title: "Title",
+      id: 1,
+      users: [
+        {
           answerId: 1,
           userId: 1,
           value: null,
           // userId_answerId: { answerId: 2, userId: 1 },
-        }]
-      }
-    )
-  })
+        },
+      ],
+    });
+  });
 
-})
+  test("connect existing on create through many side", async () => {
+    const client = await createPrismaClient({});
+    await client.folder.create({
+      data: {
+        id: "folder1",
+        name: "Folder 1",
+      },
+    });
+    await client.tag.create({
+      data: {
+        id: "tag3",
+        name: "Tag 3",
+        folders: {
+          connect: {
+            id: "folder1",
+          },
+        },
+      },
+    });
+    const folder = await client.folder.findFirst({
+      where: { id: "folder1" },
+      include: {
+        tag: true,
+      },
+    });
+    expect(folder.tag).toEqual({ id: "tag3", name: "Tag 3" });
+  });
+});
 
 describe("Update", () => {
-
-  test('createMany', async () => {
+  test("createMany", async () => {
     const client = await createPrismaClient({
-      answers: [
-        { id: 1, title: "Title" }
-      ],
+      answers: [{ id: 1, title: "Title" }],
       userAnswers: [],
-      user: [
-        { id: 1, role: "USER" }
-      ]
-    })
+      user: [{ id: 1, role: "USER" }],
+    });
     // TODO: Check output
     const answer = await client.answers.update({
       data: {
         users: {
           createMany: {
-            data: [{ userId: 1 }]
+            data: [{ userId: 1 }],
           },
-        }
+        },
       },
       where: { id: 1 },
       include: {
-        users: true
-      }
-    })
-    expect(answer).toEqual(
-      {
-        title: "Title",
-        id: 1,
-        users: [{
+        users: true,
+      },
+    });
+    expect(answer).toEqual({
+      title: "Title",
+      id: 1,
+      users: [
+        {
           answerId: 1,
           userId: 1,
-          value: null
+          value: null,
           // TODO:
           // userId_answerId: { answerId: 1, userId: 1 },
-        }]
-      }
-    )
-  })
-
-
-  test('updateMany', async () => {
-    const client = await createPrismaClient({
-      account: [
-        { id: 1, }
+        },
       ],
+    });
+  });
+
+  test("updateMany", async () => {
+    const client = await createPrismaClient({
+      account: [{ id: 1 }],
       stripe: {
         customerId: "1",
         accountId: 1,
       },
-      user: [
-        { id: 2, role: "ADMIN", accountId: 1 }
-      ]
-    })
+      user: [{ id: 2, role: "ADMIN", accountId: 1 }],
+    });
     // TODO: Check output
     const answer = await client.account.update({
       data: {
         users: {
-          updateMany: [{
-            data: {
-              role: "USER"
+          updateMany: [
+            {
+              data: {
+                role: "USER",
+              },
+              where: {
+                accountId: 1,
+              },
             },
-            where: {
-              accountId: 1,
-            }
-          }]
-        }
+          ],
+        },
       },
       where: {
-        id: 1
+        id: 1,
       },
       include: {
-        users: true
-      }
-    })
-    expect(answer).toEqual(
-      {
-        id: 1,
-        users: [{
+        users: true,
+      },
+    });
+    expect(answer).toEqual({
+      id: 1,
+      users: [
+        {
           id: 2,
           accountId: 1,
-          role: "USER"
-        }]
-      }
-    )
-  })
-
-
-  test('update', async () => {
-    const client = await createPrismaClient({
-      account: [
-        { id: 1, }
+          role: "USER",
+        },
       ],
+    });
+  });
+
+  test("update", async () => {
+    const client = await createPrismaClient({
+      account: [{ id: 1 }],
       stripe: {
         customerId: "1",
         accountId: 1,
       },
-      user: [
-        { id: 2, role: "ADMIN", accountId: 1 }
-      ]
-    })
+      user: [{ id: 2, role: "ADMIN", accountId: 1 }],
+    });
     // TODO: Check output
     const answer = await client.account.update({
       data: {
         users: {
-          update: [{
-            data: {
-              role: "USER"
+          update: [
+            {
+              data: {
+                role: "USER",
+              },
+              where: {
+                id: 2,
+              },
             },
-            where: {
-              id: 2,
-            }
-          }]
-        }
+          ],
+        },
       },
       where: {
-        id: 1
+        id: 1,
       },
       include: {
-        users: true
-      }
-    })
-    expect(answer).toEqual(
-      {
-        id: 1,
-        users: [{
+        users: true,
+      },
+    });
+    expect(answer).toEqual({
+      id: 1,
+      users: [
+        {
           id: 2,
           accountId: 1,
-          role: "USER"
-        }]
-      }
-    )
-  })
+          role: "USER",
+        },
+      ],
+    });
+  });
 
   test("deleteMany array", async () => {
     const client = await createPrismaClient({
-      account: [
-        { id: 1, name: "A" }
+      account: [{ id: 1, name: "A" }],
+      user: [
+        {
+          id: 2,
+          accountId: 1,
+        },
+        {
+          id: 3,
+          accountId: 1,
+        },
       ],
-      user: [{
-        id: 2,
-        accountId: 1,
-      }, {
-        id: 3,
-        accountId: 1,
-      }],
-    })
+    });
 
     await client.account.update({
       data: {
         users: {
-          update: [{ where: {id: 1}, name: "Piet" }],
-          deleteMany: [
-            { id: 2 } ,
-          ]
-        }
+          update: [{ where: { id: 1 }, name: "Piet" }],
+          deleteMany: [{ id: 2 }],
+        },
       },
       where: {
-        id: 1
+        id: 1,
       },
       include: {
-        users: true
-      }
-    })
+        users: true,
+      },
+    });
     const account = await client.account.findUnique({
       where: {
-        id: 1
+        id: 1,
       },
       include: {
-        users: true
-      }
-    })
-    expect(account.users.length).toEqual(1)
-  })
+        users: true,
+      },
+    });
+    expect(account.users.length).toEqual(1);
+  });
 
   test("deleteMany object", async () => {
     const client = await createPrismaClient({
-      account: [
-        { id: 1, name: "A" }
+      account: [{ id: 1, name: "A" }],
+      user: [
+        {
+          id: 2,
+          accountId: 1,
+        },
+        {
+          id: 3,
+          accountId: 1,
+        },
       ],
-      user: [{
-        id: 2,
-        accountId: 1,
-      }, {
-        id: 3,
-        accountId: 1,
-      }],
-    })
+    });
 
     await client.account.update({
       data: {
         users: {
-          deleteMany: 
-            { id: 2 } ,
-        }
+          deleteMany: { id: 2 },
+        },
       },
       where: {
-        id: 1
+        id: 1,
       },
       include: {
-        users: true
-      }
-    })
+        users: true,
+      },
+    });
     const account = await client.account.findUnique({
       where: {
-        id: 1
+        id: 1,
       },
       include: {
-        users: true
-      }
-    })
-    expect(account.users.length).toEqual(1)
-  })
+        users: true,
+      },
+    });
+    expect(account.users.length).toEqual(1);
+  });
 
   test("delete array", async () => {
     const client = await createPrismaClient({
-      account: [
-        { id: 1, name: "A" }
+      account: [{ id: 1, name: "A" }],
+      user: [
+        {
+          id: 2,
+          accountId: 1,
+        },
+        {
+          id: 3,
+          accountId: 1,
+        },
       ],
-      user: [{
-        id: 2,
-        accountId: 1,
-      }, {
-        id: 3,
-        accountId: 1,
-      }],
-    })
+    });
 
     await client.account.update({
       data: {
         users: {
-          update: [{ where: {id: 1}, name: "Piet" }],
-          delete: [
-            { id: 2 } ,
-          ]
-        }
+          update: [{ where: { id: 1 }, name: "Piet" }],
+          delete: [{ id: 2 }],
+        },
       },
       where: {
-        id: 1
+        id: 1,
       },
       include: {
-        users: true
-      }
-    })
+        users: true,
+      },
+    });
     const account = await client.account.findUnique({
       where: {
-        id: 1
+        id: 1,
       },
       include: {
-        users: true
-      }
-    })
-    expect(account.users.length).toEqual(1)
-  })
+        users: true,
+      },
+    });
+    expect(account.users.length).toEqual(1);
+  });
 
   test("delete object", async () => {
     const client = await createPrismaClient({
-      account: [
-        { id: 1, name: "A" }
+      account: [{ id: 1, name: "A" }],
+      user: [
+        {
+          id: 2,
+          accountId: 1,
+        },
+        {
+          id: 3,
+          accountId: 1,
+        },
       ],
-      user: [{
-        id: 2,
-        accountId: 1,
-      }, {
-        id: 3,
-        accountId: 1,
-      }],
-    })
+    });
 
     await client.account.update({
       data: {
         users: {
-          delete: 
-            { id: 2 } ,
-        }
+          delete: { id: 2 },
+        },
       },
       where: {
-        id: 1
+        id: 1,
       },
       include: {
-        users: true
-      }
-    })
+        users: true,
+      },
+    });
     const account = await client.account.findUnique({
       where: {
-        id: 1
+        id: 1,
       },
       include: {
-        users: true
-      }
-    })
-    expect(account.users.length).toEqual(1)
-  })
-
-})
+        users: true,
+      },
+    });
+    expect(account.users.length).toEqual(1);
+  });
+});
 
 test("Select", async () => {
   const client = await createPrismaClient({
     account: [
-      { id: 1, name: "A", },
-      { id: 2, name: "B", },
+      { id: 1, name: "A" },
+      { id: 2, name: "B" },
     ],
-    user: [
-      { id: 1, role: "USER", accountId: 1, delete: true },
-    ]
-  })
+    user: [{ id: 1, role: "USER", accountId: 1, delete: true }],
+  });
 
   const accounts = await client.account.findMany({
     where: {
-      users: { delete: true }
-    }
-  })
-  expect(accounts).toHaveLength(1)
-})
+      users: { delete: true },
+    },
+  });
+  expect(accounts).toHaveLength(1);
+});
